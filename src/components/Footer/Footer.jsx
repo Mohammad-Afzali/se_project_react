bc1d6a6f36f986ac7676b0ac25c505f7d0ef5d63
Mohammad Afzali <afzali.mamoon@gmail.com>
2024-09-14 13:42:03 -0400

    redo repo

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer">
+        <p className="footer__author">Mohammad Afzali</p>
+        <p className="footer__copyright">{new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,35 @@
+import "./WeatherCard.css"
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
+
+function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condation === weatherData.condation
+    );
+  });
+
+  let weatherOption;
+  if (filteredOptions.lenght === 0) {
+    weatherOptions = defaultWeatherOptions[weatherData.isDay ? "day" : "night"];
+  } else {
+    weatherOption = filteredOptions[0];
+
+  }
+
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">
+        {weatherData.temp.F} &deg; F
+      </p>
+      <img
+        src={weatherOption?.url}
+        alt={`Card showing ${weatherOption?.day ? "day" : "night"} time ${weatherOption?.Condition 
+        } weather`}
+        className="weather-card__image"
+      />
+    </section>
+  );
+}
+
+export default WeatherCard;

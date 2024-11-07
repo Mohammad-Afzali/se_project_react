1cddce9b5de852fc2ac3122911be1ece4e3a1c5f
Mohammad Afzali <afzali.mamoon@gmail.com>
2024-11-07 13:49:14 -0500

    update comments

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -15,13 +15,7 @@ export default function DeleteModal({
   return (
     <div className={`modal ${isOpened && "modal_opened"}`}>
       <div className="modal__container modal__content_type_delete modal__content">
-        <button className="modal__close" type="button">
-          <img
-            src={close}
-            alt="close-button"
-            className="modal__close-image"
-            onClick={handleCloseClick}
-          />
+        <button className="modal__close" type="button" onClick={handleCloseClick}>
         </button>
         <p className="modal__delete_question">
           Are you sure you want to delete this item?
